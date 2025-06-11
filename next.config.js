ce81f0df9e88d6685d589abd4a62f221540439ee
Farelart <ffjdganlaky@gmail.com>
2025-06-11 19:38:27 +0100

    config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["media.giphy.com", "example.com"], // Add other existing domains here
-  },
-};
-
-module.exports = nextConfig;
